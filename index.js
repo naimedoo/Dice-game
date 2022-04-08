8f51cb4f6c12e1a1b4ef4f2492f889437470f081
Naim EDOO <Naim.e2703@gmail.com>
2022-04-08 07:23:41 +0200

    ProjectDice game

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 
-   let init = document.getElementById('newGame')
-   let player1 = document.querySelector('.player1');
-   let player2 = document.querySelector('.player2');
-   let btnRoll = document.getElementById('.Roll');
-   let img = document.querySelector('.imgD');
+let init = document.getElementById('newGame')
+let player1 = document.querySelector('.player1');
+let player2 = document.querySelector('.player2');
+let btnRoll = document.getElementById('.Roll');
+let img = document.querySelector('.imgD');
+
+
    
-   let activePlayer = player1;
-   let games;
-   
 
+//---------------------  initialiser la partie ---------------------------//
 
+newGame.addEventListener("click", ()  => { 
 
-//  initialiser la partie
- newGame.addEventListener("click", ()  => { 
          player1 = prompt("joueur 1, quel est votre pseudo ?");
          player2 = prompt("joueur 2, quel est votre pseudo ?");
          if (confirm("nouvelle partie ?") == true) {  
          document.querySelector('.player1').innerHTML = player1;
          document.querySelector('.player2').innerHTML = player2;
-         blockPlayer.style.opacity = 1;
+         //blockPlayer.style.opacity = 1;
          blockPlayer.style.background= "white";
-         roundPlayerOne.textContent = "0";
-         roundPlayerTwo.textContent = "0";
-         totalP1.textContent = "0";
-         totalP2.textContent = "0";
-         games = true;
+         scoreP1.textContent = "0";
+         scoreP2.textContent = "0";
+         current1.textContent = "0";
+         current2.textContent = "0";
          result.innerHTML="";
+         if(activePlayer == player1) {
+            changePlayer();
+         }
         } 
-     })
+})
+     
 
-//  au clic du bouton lancé le dé
+//---------------------------- au clic du bouton lancé le dé --------------------//
+
+let current1 = document.getElementById('roundPlayerOne')
+let current2 = document.getElementById('roundPlayerTwo')
+let activePlayer = player1;
 
 roll.addEventListener("click", () => {
    let diceFaces = [
@@ -40,55 +46,100 @@ roll.addEventListener("click", () => {
       "imgDice/dice5.png",
       "imgDice/dice6.png"
    ];
-   
-   //img.style.visibility = "visible";
    let ValueDice = Math.floor(Math.random() * diceFaces.length) + 1;
    let dice = diceFaces[ValueDice - 1];
    img.setAttribute('src', dice)
-  if(ValueDice == 1) {
+   if(ValueDice == 1) {
       if(activePlayer == player1 ) {
-         roundPlayerOne.textContent = 0;
+         current1.textContent = 0;
       } else {
-         roundPlayerTwo.textContent = 0;
+         current2.textContent = 0;
       }
       changePlayer();
    } else  {
       if(activePlayer == player1) {
-         roundPlayerOne.textContent = parseInt(roundPlayerOne.textContent) + ValueDice;
+         current1.textContent = parseInt(current1.textContent) + ValueDice;
       } else {
-         roundPlayerTwo.textContent = parseInt(roundPlayerTwo.textContent) + ValueDice;
+         current2.textContent = parseInt(current2.textContent) + ValueDice;
       }
    }
-})
-const changePlayer = () => {
-   activePlayer.classList.remove("on");
-   activePlayer.classList.add("off");
-   activePlayer = activePlayer = player1 ? player2 : player1;
-   activePlayer.classList.remove("off");
-   activePlayer.classList.add("on");
-}
+});
 
+// changement de joueur 
 
-// bouton pou stocker les points
-let scoreP1 = document.getElementById("totalP1");
-let scoreP2 = document.getElementById("totalP2");
-const holdDice = document.getElementById("hold");
-let result = document.querySelector('.result');
-
-const store = () => {
-   if(activePlayer == player1) {
-      scoreP1.textContent = parseInt(scoreP1.textContent) + parseInt(roundPlayerOne.textContent);
-      roundPlayerOne.textContent = 0;
-      scoreP1.textContent >= 100 ? endGame : changePlayer();
-   } else {
-      scoreP2.textContent = parseInt(scoreP2.textContent) + parseInt(roundPlayerTwo.textContent);
-      roundPlayerTwo.textContent = 0;
-      scoreP2.textContent >= 100 ? endGame : changePlayer();
+function  changePlayer() {
+   if(activePlayer == "1") {
+      blockPlayer.style.opacity = "1";
+      blockPlayerTwo.style.opacity = "0";
+   } else  {
+      blockPlayer.style.opacity = "0";
+      blockPlayerTwo.style.opacity = "1";
    }
 }
 
-//  fin du jeu
 
+
+
+
+
+
+
+
+
+
+
+
+// //   const changePlayer = () => {
+
+// //     activePlayer.classList.remove("player1");
+// //     activePlayer.classList.add("player2");
+// //     activePlayer = activePlayer = player1 ? player2 : player1;
+// //     activePlayer.classList.remove("player2");
+// //     activePlayer.classList.add("player1");
+    
+    
+// //   }
+
+
+// // bouton pour stocker les points
+
+// let scoreP1 = document.getElementById("totalP1");
+// let scoreP2 = document.getElementById("totalP2");
+// const holdDice = document.getElementById("hold");
+// let result = document.querySelector('.result');
+
+// const store = () => {
+//    if(activePlayer == player1) {
+//       scoreP1.textContent = parseInt(scoreP1.textContent) + parseInt(scoreP1.textContent);
+//       current1.textContent = 0;
+//       scoreP1.textContent >= 100 ? endGame : changePlayer();
+//    } else {
+//       scoreP2.textContent = parseInt(scoreP2.textContent) + parseInt(scoreP2.textContent);
+//       current2.textContent = 0;
+//       scoreP2.textContent >= 100 ? endGame : changePlayer();
+//    }
+// }
+
+// // changement de joueur
+
+// function nextRound() {
+//    if(activePlayer === 1) {   
+//       player1.style.opacity == "1";
+//       player2.style.opacity == "0";
+//     } else  {
+//       player1.style.opacity == "0";
+//       player2.style.opacity == "1";
+//     }
+// }
+
+// //  fin du jeu
+// let play = document.getElementById('blockplayer')
+// const end = () => {
+//    activePlayer = (activePlayer == player1) ? player1 : player2;
+//    play.innerHTML =(activePlayer == player1 ? player1.id : player2.id ) + " a gagné !";
+
+// }
+// holdDice.addEventListener("click", store);
 
      
 
